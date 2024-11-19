b2d80e9510ed19a7edca040ed6e9e49b8268e369
mariluog <179652193+mariluog@users.noreply.github.com>
2024-11-19 20:08:50 -0300

    Update webapp00.py

--- pages/webapp00.py
+++ pages/webapp00.py
@@ -1,11 +1,5 @@
 # MEU PRIMEIRO WEB APP
 import streamlit as st
-from ACTlib01 import *
-
-#url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQFwxxM13bxUC0dpyd0w0PxfZIrJ-hp4Px-R6rsTiG3c3n-89JApzA0jYJpU9vNfxeNCvtJ0Cg35KtO/pub?gid=556192647&single=true&output=csv"
-#db = Ler_GooglePlanilha(url)
-#db.fillna('', inplace=True)
-#Escrever(db)
 
 # Use st.title("") para adicionar um TÍTULO ao seu Web app
 st.title("MEU 1º WEB APP STREAMLIT")
@@ -21,5 +15,3 @@ st.write("Como já deve ter percebido, o método st.write() é usado para escrit
 
 values = st.slider("Select a range of values", 0.0, 100.0, (5.0, 15.0))
 st.write("Values:", values)
-
-st.image("desenvolvimento.jpg", caption="TESTE_Inserir_IMAGEM")
